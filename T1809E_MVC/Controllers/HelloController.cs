8bbf08355fd2010a3d14438d4338e4bbee128e9c
Dao Linh <luyendh@fpt.edu.vn>
2020-01-16 20:03:49 +0700

    Basic demo.

---
+++ T1809E_MVC/Controllers/HelloController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace T1809E_MVC.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: Hello
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Future()
+        {
+            return View();
+        }
+
+        public ActionResult World()
+        {
+            return View("Future");
+        }
+    }
+}
---
+++ T1809E_MVC/Controllers/StudentController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using T1809E_MVC.Models;
+
+namespace T1809E_MVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        private static List<Student> list = new List<Student>();
+
+        // GET: Student
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public  ActionResult Store(string fullName, string password)
+        {
+            Debug.WriteLine(fullName);
+            Debug.WriteLine(password);
+            var student = new Student()
+            {
+                FullName = fullName,
+                Password = password
+            };
+            list.Add(student);
+            ViewBag.CurrentStudent = student;
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            ViewBag.ListStudent = list;
+            return View();
+        }
+
+    }
+}
